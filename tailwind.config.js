b92deae211b24473b42c98e822b69270387fe085
DESKTOP-Q3AT01B\huina <hui.nathan9@gmail.com>
2022-09-17 16:48:26 +0100

    add react icons to project, configure tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "576",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
