14b183c817a53877f6be0ab42242d12dbd73d988
Nathan Hui <hui.n1998@gmail.com>
2022-09-20 15:36:49 +0100

    sidebar component,

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 const Layout: NextPage<LayoutProps> = ({ children }) => (
   <div>
     <Header />
-    <div className="m-auto max-w-[90rem] px-4 md:px-8">{children}</div>
+    <div className="m-auto max-w-[90rem] px-4 pt-20 md:px-8">{children}</div>
   </div>
 );
 
---
+++ src/constants/data-html.ts
@@ -0,0 +1,13 @@
+export const SIDEBAR_LINKS = [
+  {
+    id: 0,
+    heading: "HTML",
+    links: [
+      {
+        slug: "html-elements",
+        text: "Elements",
+        href: "/html/elements",
+      },
+    ],
+  },
+];
